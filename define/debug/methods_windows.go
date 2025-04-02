44c026ce91d4a9df26316f07d5873a1de3f7c24c
Administrator <7@chaksunshine.com>
2025-04-03 00:11:43 +0800

    update

--- define/debug/methods_windows.go
+++ define/debug/methods_windows.go
@@ -9,6 +9,10 @@ import (
 // 标记项目不允许发布
 // @param data 选项信息
 func NotRelease(data ...interface{}) error {
+	if IsWindows == false {
+		return errors.New("当前非windows环境,不允许发布")
+	}
+
 	for _, datum := range data {
 		catLog.Error(fmt.Sprintf("项目收到不允许发布标记 %#v", datum))
 	}
--- define/debug/var.go
+++ define/debug/var.go
@@ -1,4 +1,12 @@
 package debug
 
-// 是否是生产模式
-var IsRelease = false
+import (
+	"os"
+	"runtime"
+)
+
+// 当前是否是windows平台
+var IsWindows = runtime.GOOS == `windows`
+
+// 检查是否是在idea中运行环境
+var IsIdea = os.Getenv(`JB_GOLAND`) == `dFX06CJErVG2DOL179nAHzmQTNxbtkWy5ulRSUesgafwi3MZPI4Kqcjv8ohpYBlr`
