60115c145124820e940e9ab557fdf3d255a477e8
Administrator <7@chaksunshine.com>
2025-04-12 21:22:36 +0800

    update

--- numbers/convert.go
+++ numbers/convert.go
@@ -1,6 +1,9 @@
 package numbers
 
-import "strconv"
+import (
+	"math"
+	"strconv"
+)
 
 // 将字符串转换成int
 // @param str 字符串
@@ -34,4 +37,33 @@ func StringToFloat64Must(str string) float64 {
 func StringToFloat32Must(str string) float32 {
 	atoi, _ := strconv.ParseFloat(str, 32)
 	return float32(atoi)
+}
+
+// 将一个数字四舍五入格式化
+// @param value 数字
+// @param length 保留小数位数
+func Float64Format(value float64, length int) float64 {
+
+	var speed float64
+
+	if length == 1 {
+		speed = 10
+	}
+	if length == 2 {
+		speed = 100
+	}
+	if length == 3 {
+		speed = 1000
+	}
+	if length == 4 {
+		speed = 10000
+	}
+	if length == 5 {
+		speed = 100000
+	}
+
+	if speed > 0 {
+		value = math.Round(value*speed) / speed
+	}
+	return value
 }
